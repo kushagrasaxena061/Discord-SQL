e6d988d5ca21d251c0cf15f664a8b2d8bd55a3b0
Kushagra Saxena <faaltuwebsitekeliye@gmail.com>
2023-08-31 18:11:28 +0530

    app complete

--- lib/initial-profile.ts
+++ lib/initial-profile.ts
@@ -1,9 +1,32 @@
-import { PrismaClient } from "@prisma/client";
+import { currentUser, redirectToSignIn } from "@clerk/nextjs";
 
-declare global {
-  var prisma: PrismaClient | undefined;
+import { db } from "@/lib/db";
+
+export const initialProfile = async () => {
+  const user = await currentUser();
+
+  if (!user) {
+    return redirectToSignIn();
+  }
+
+  const profile = await db.profile.findUnique({
+    where: {
+      userId: user.id
+    }
+  });
+
+  if (profile) {
+    return profile;
+  }
+
+  const newProfile = await db.profile.create({
+    data: {
+      userId: user.id,
+      name: `${user.firstName} ${user.lastName}`,
+      imageUrl: user.imageUrl,
+      email: user.emailAddresses[0].emailAddress
+    }
+  });
+
+  return newProfile;
 };
-
-export const db = globalThis.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db
